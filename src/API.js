95c8f8ec1c60ef55b5bdc0294fccf67fc7fc8072
PHUTHIPHONG MONGKHON <james_apple@hotmail.co.th>
2020-09-23 17:18:03 +0700

    change api

--- src/API.js
+++ src/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const API = axios.create({
-  baseURL: `http://127.0.0.1:8000/api`,
+  baseURL: `https://online-registration-backend.herokuapp.com/api`,
   // headers: {
   //   Authorization: 'Bearer {token}'
   // }
